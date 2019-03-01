be95cac8cf8c5199b9eb985df84db16a37238c59
candye_sweet <olyamelska@dyn-209-2-213-91.dyn.columbia.edu>
2019-02-28 21:07:48 -0500

    "Grabbing stuff"

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let backgroundArray = [
+  "../images/background1.jpg",
+  "../images/background2.jpg",
+  "../images/background3.jpg",
+  "../images/background4.jpg"
+];
+
+document.on("load", event => {});
