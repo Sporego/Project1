766f4fee4fd5b8aced134160cc9b8311cdb0beb4
candye_sweet <olyamelska@dyn-209-2-212-233.dyn.columbia.edu>
2019-03-02 16:31:31 -0500

    Adding News Bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,12 @@ let backgroundArray = [
   "../images/background4.jpg"
 ];
 let city = "New York";
+let satrtDate;
+let endDate;
+let APIkeyNews = "rVruAxBlYzZYu9EleoRs0Vbwvuo0NZ6c";
+let queryURLNews =
+  "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:Culture&Arts&Museums&glocations:NEW+YORK+CITY&sort=newest&api-key=" +
+  APIkeyNews;
 
 let APIkeyWeather = "70c19500496f9aecbd97ff861d3072ee";
 let queryURLWeather =
@@ -14,10 +20,43 @@ let queryURLWeather =
   APIkeyWeather;
 
 $.ajax({
+  url: queryURLNews,
+  method: "GET"
+}).then(function(news) {
+  console.log(queryURLNews);
+
+  console.log(news);
+  let response = news.response;
+  for (let i = 0; i < response.docs.length; i++) {
+    let cardDivNews = $("<div>").attr("class", "card");
+    let imgNews = $("<img>")
+      .attr(
+        "src",
+        "https://static01.nyt.com/" + response.docs[i].multimedia[0].url
+      )
+      .attr("class", "img-fluid");
+    let cardBodyDivNews = $("<div>").attr("class", "card-body");
+    let cardTitleNews = $("<h5>")
+      .text(response.docs[i].headline.main)
+      .attr("class", "card-title");
+    let cardTextNews = $("<p>")
+      .text(response.docs[i].snippet)
+      .attr("class", "card-text");
+
+    let linkNews = $("<a>")
+      .attr("href", response.docs[i].web_url)
+      .attr("class", "btn btn-dark btn-news")
+      .text("Read Article");
+    cardBodyDivNews.append(cardTitleNews, cardTextNews, linkNews);
+    cardDivNews.append(imgNews, cardBodyDivNews);
+    $("#displayNews").append(cardDivNews);
+  }
+});
+
+$.ajax({
   url: queryURLWeather,
   method: "GET"
 }).then(function(response) {
-  console.log(response);
   let temperatureAndCity = $("<p>").text(
     response.name.toUpperCase() + ": " + response.main.temp + "° F "
   );
@@ -25,7 +64,6 @@ $.ajax({
   let arr = [];
   for (let i = 0; i < response.weather.length; i++) {
     arr.push(response.weather[i].icon);
-    console.log(arr);
   }
   for (let i = 0; i < arr.length; i++) {
     let imgWeather = $("<img>").attr(
